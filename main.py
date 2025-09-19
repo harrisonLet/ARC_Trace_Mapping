df0f284b1cdc964d1c1964cf3100138e929ab3c7
harrisonLet <harrison.letourneau@gmail.com>
2025-09-19 01:24:24 -0600

    - Added satellite and topo map selection - Added new layers    - CH4, CO2, C2H6, C2C1 mixing ratio, delta13C    - Added wind vector mapping (lowered data resolution -- every 25 sample points -- to improve and maintain leaflet operation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Author: Harrison LeTourneau
 
 An interactive geospatial mapping of NOAA's 2024 USOS Study of Salt Lake City's GHG Pollutants
 
-DATA FIELDS:
+ARC DATA FIELDS:
 
 lat_DGPS_deg, degrees, Latitude in decimal by Hemisphere VS1100 Differential GPS
 lon_DGPS_deg, degrees, Longitude in decimal by Hemisphere VS1100 Differential GPS
@@ -45,52 +45,53 @@ BC950_AE43_ng_m3, ng/m3, Black Carbon at 950 nm by AE43
 PM25, ug/m3, PM2.5 by particle sensor
 PM10, ug/m3, PM10 by particle sensor
 Valve, boolian, indicator of valve conditions;0 is measurement; 10 is zeroing; 11 is spanning
+
+* many are naan -9999
 """
 
 import pandas as pd
 import folium
 import branca.colormap as cm
 import numpy as np
-import icartt
-from branca.colormap import LinearColormap
+from tqdm import tqdm
 
 
 def main():
-    print("RUNNING")
 
-    # 718 is very large
-    file_name = "ARC/USOS-ARL-Suite_ARC_20240718_RA.ict"
+    # 718 730
+    file_name = "ARC/USOS-ARL-Suite_ARC_20240801_RA.ict"
+
+    print("Generating ARC mapping for: " + file_name)
 
     # Pandas dataframe
     arc_data = arc_data_dataframe(file_name)
 
-    # ICARTT dataset
-    arc_data2 = arc_data_icartt(file_name)
-
     # ARC map with car path
-    m = arc_map(arc_data)
-
-    print("Created Mapping")
+    m = arc_map(arc_data, file_name)
 
     # Add Layers
     add_layer(m, arc_data, 'CH4_aeris313_ppm')
     add_layer(m, arc_data, 'H2O_aeris313_ppm')
     add_layer(m, arc_data, 'CO2_g2401m_ppm')
+    add_layer(m, arc_data, 'alt_msl_m')
+
+    add_layer(m, arc_data, 'C2H6_aeris313_ppb')
+    add_layer(m, arc_data, 'C2C1_aeris313')
+    add_layer(m, arc_data, 'delta13C_CH4_raw')
+
+    # Add Vector map
+    add_vector_map(m, arc_data, 'true_WS_m_s')
 
     # Add layer control
     folium.LayerControl().add_to(m)
 
+    print("Generating html file...")
+
     # Save to html
-    m.save('slc_car_path.html')
+    m.save('folium/slc_car_path.html')
 
+    print('Successfully saved file.')
 
-
-def arc_data_icartt(filepath):
-    """Reads an ICARTT ARC file using python ICARTT package"""
-
-    ict_dataset = icartt.Dataset(filepath)
-
-    return ict_dataset
 
 def arc_data_dataframe(filepath):
     """
@@ -107,6 +108,7 @@ def arc_data_dataframe(filepath):
                 header_line_index = i
                 break
 
+    print("Creating dataframe...")
     # Read the file with pandas
     df = pd.read_csv(
         filepath,
@@ -121,8 +123,10 @@ def arc_data_dataframe(filepath):
 
     return df
 
-def arc_map(ds):
+def arc_map(ds, filename):
 
+    print("Generating map...")
+    m = folium.Map()
     #Retrieve lat and lon data cols
     lat_col = ds['lat_DGPS_deg']
     lon_col = ds['lon_DGPS_deg']
@@ -131,7 +135,24 @@ def arc_map(ds):
     coords = list(zip(lat_col, lon_col))
 
     # Center map on mean location
-    m = folium.Map(location=[lat_col.mean(), lon_col.mean()], zoom_start=12, prefer_canvas=True)
+    m = folium.Map(location=[lat_col.mean(), lon_col.mean()], zoom_start=12, prefer_canvas=True, tiles=False)
+
+    # Street, topo, satellite
+    folium.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+	                attr='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+                    name='Topo',
+                    control=True,
+                    overlay=False
+                     ).add_to(m)
+
+    folium.TileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png',
+                     attr='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                     name='Satellite',
+                     control=True,
+                     overlay=False
+                     ).add_to(m)
+
+    folium.TileLayer("OpenStreetMap", name='StreetMap', control=True, overlay=False).add_to(m)
 
     # Add the car's path as a blue polyline
     folium.PolyLine(coords, color="blue", weight=3, opacity=0.7).add_to(m)
@@ -143,6 +164,7 @@ def add_layer(map_obj, df, column):
     if column not in df.columns:
         return
 
+    print("Adding layer {}...".format(column))
     layer = folium.FeatureGroup(name=column, control=True, show=False)
 
     # Get robust min/max
@@ -150,7 +172,7 @@ def add_layer(map_obj, df, column):
     rob_max = df[column].quantile(0.99)
 
     # Color map
-    linear = cm.linear.magma.scale(rob_min, rob_max)
+    linear = cm.linear.inferno.scale(rob_min, rob_max)
     linear.caption = column
 
     # Retrieve lat and lon data cols
@@ -160,36 +182,86 @@ def add_layer(map_obj, df, column):
     # Transform to tuples for folium
     coords = list(zip(lat_col, lon_col))
 
-    print("Adding ", column, " layer")
-
     # Use Color line for faster rendering
     folium.ColorLine(
         name=column,
         positions=coords,
-        colors=df[column],
+        colors=df[column].astype(float),
         colormap=linear,
         weight=14,
-        opacity=0.9
+        opacity=0.8
     ).add_to(layer)
-
 
     # Use circle markers for popups with exact location value
 
-    # for _, row in df.iterrows():
+    # for _, row in tqdm(df.iterrows(), total=len(df), bar_format='{bar}', colour='white'):
     #     value = row[column]
     #
     #     if pd.notna(value):
-    #         folium.Circle(
+    #         folium.CircleMarker(
     #             location=(row["lat_DGPS_deg"], row["lon_DGPS_deg"]),
-    #             radius=50,
-    #             # color=linear(value),
-    #             show=False,
+    #             radius=20,
+    #             color=linear(value),
+    #             show=True,
     #             fill=True,
-    #             fill_opacity=0.9,
+    #             fill_opacity=0.8,
     #             stroke=False,
     #             popup=folium.Popup(f"{column}: {value:.1f} @ {row['StartTime_seconds']}")  # ADD in UTC time for current row
     #         ).add_to(layer)
 
+
+    # Add colormap key
+    map_obj.add_child(linear)
+
+    layer.add_to(map_obj)
+
+def add_vector_map(map_obj, df, column):
+
+    layer = folium.FeatureGroup(name=column, control=True, show=False)
+
+    # Get robust min/max
+    rob_min = df[column].quantile(0.01)
+    rob_max = df[column].quantile(0.99)
+
+    # Color map
+    linear = cm.linear.RdBu_04.scale(rob_min, rob_max)
+    linear.caption = column
+
+    # Retrieve lat and lon data cols
+    lat_col = df['lat_DGPS_deg']
+    lon_col = df['lon_DGPS_deg']
+
+    # Transform to tuples for folium
+    coords = list(zip(lat_col, lon_col))
+
+    print("Adding ", column, " vector map...")
+
+
+    for idx, row in tqdm(df.iloc[::25].iterrows(), total=len(df[::25]), bar_format='{bar}', colour='white'):
+        lat = row['lat_DGPS_deg']
+        lon = row['lon_DGPS_deg']
+        ws = row['true_WS_m_s']
+        wd = row['true_WD_deg']
+
+        # Map color to wind speed
+        color = linear(ws) if pd.notna(ws) else '#888888'
+
+        folium.Marker(
+            location=(lat, lon),
+            icon=folium.DivIcon(
+                html=f"""
+                    <div style="
+                        font-size:30px;
+                        font-weight:bold;
+                        opacity:0.8;
+                        transform: rotate({wd}deg);
+                        color: {color};
+                        ">
+                        &#11014;
+                    </div>
+                """
+            )
+        ).add_to(layer)
 
     # Add colormap key
     map_obj.add_child(linear)
